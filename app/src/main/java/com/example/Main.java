b7f5058aa72271d7e153c62f6470f30e7ae54cba
KAUE DAVI ROCHA DE JESUS <kaue.jesus7@senaisp.edu.br>
2026-09-15 14:45:30 -0300

    Feat: adicionando variaveis

--- app/src/main/java/com/example/Main.java
+++ app/src/main/java/com/example/Main.java
@@ -1,7 +1,33 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner env = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String username = env.nextLine();
+        float valor = 0f;
+        int quantidade =0;
+
+        float total = 0f;
+        float maiorNumero = 0f;
+        float menorNumero = 0f;    
+
+        int caso = 1;
+
+        do {
+            
+            System.out.println("Digite o valor da compra:");
+            valor = env.nextFloat();
+            quantidade++;
+
+
+            System.out.println("Deseja continuar? (1 - Sim, 2 - Não):");
+            caso = env.nextInt();
+        } while (caso != 1);
+
+
     }
 }
